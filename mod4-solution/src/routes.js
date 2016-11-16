c090038b1598ec20160df08d584ca149ab009709
Brooklyn Photos <guoneng.list@gmail.com>
2016-11-15 22:09:33 -0500

    mod4

---
+++ mod4-solution/src/routes.js
@@ -0,0 +1,47 @@
+(function () {
+'use strict';
+
+angular.module('MenuApp')
+.config(RoutesConfig);
+
+RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+function RoutesConfig($stateProvider, $urlRouterProvider) {
+
+  // Redirect to home page if no other URL matches
+  $urlRouterProvider.otherwise('/');
+
+  // *** Set up UI states ***
+  $stateProvider
+
+  // Home page
+  .state('home', {
+    url: '/',
+    templateUrl: 'src/menuapp/templates/home.template.html'
+  })
+
+  // Categories
+  .state('categories', {
+    url: '/categories',
+    templateUrl: 'src/menuapp/templates/categories.state.template.html',
+    controller: 'CategoriesController as catCtrl',
+    resolve: {
+      categories: ['MenuDataService', function (MenuDataService) {
+        return MenuDataService.getAllCategories();
+      }]
+    }
+  })
+
+  .state('items', {
+      url: '/{categoryShortName}/items',
+      templateUrl: 'src/menuapp/templates/items.template.html',
+      controller: 'ItemsController as itemsCtrl',
+      resolve: {
+        items: ['MenuDataService','$stateParams', function (MenuDataService,$stateParams) {
+          return MenuDataService.getItemsForCategory($stateParams.categoryShortName);
+        }]
+      }
+  });
+
+}
+
+})();
